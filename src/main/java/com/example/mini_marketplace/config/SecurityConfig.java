b61226a839875a289ca1b71a1d6fb4a50ec8be7b
Adit-Mugdha-Das <mailbox.mugdha@gmail.com>
2026-03-26 10:04:36 +0600

    Add REST API updates again

--- src/main/java/com/example/mini_marketplace/config/SecurityConfig.java
+++ src/main/java/com/example/mini_marketplace/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -91,6 +92,7 @@ public class SecurityConfig {
                     new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED),
                     new AntPathRequestMatcher("/api/**")
                 )
+                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/auth/login"))
             )
             .formLogin(form -> form
                 .loginPage("/auth/login")
